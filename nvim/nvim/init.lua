13302535fdada8e2268767ac74677715b14af16f
Giovan Isa Musthofa <giovanmail@gmail.com>
2022-02-19 11:26:39 +0700

    nvim: Refactor which-key keybindings
    Signed-off-by: Giovan Isa Musthofa <giovanmail@gmail.com>

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -19,15 +19,15 @@ local wk = require('which-key')
 wk.register({
   -- <leader>
   ['<leader>'] = {
-    e = {'<cmd>NvimTreeToggle<CR>', ':NvimTreeToggle', noremap = true},
+    e = {'<cmd>NvimTreeToggle<CR>', ':NvimTreeToggle'},
   },
   -- fzf-lua mappings
   ['<C-p>'] = {
     name = '+fzf-lua',
-    b = {"<cmd>lua require('fzf-lua').buffers()<CR>", 'open buffers', noremap = true, silent = true},
-    f = {"<cmd>lua require('fzf-lua').files()<CR>", 'find or fd on a path', noremap = true, silent = true},
-    g = {"<cmd>lua require('fzf-lua').live_grep()<CR>", 'live grep current project', noremap = true, silent = true},
-    h = {"<cmd>lua require('fzf-lua').oldfiles()<CR>", 'opened files history', noremap = true, silent = true},
+    b = {"<cmd>lua require('fzf-lua').buffers()<CR>", 'open buffers'},
+    f = {"<cmd>lua require('fzf-lua').files()<CR>", 'find or fd on a path'},
+    g = {"<cmd>lua require('fzf-lua').live_grep()<CR>", 'live grep current project'},
+    h = {"<cmd>lua require('fzf-lua').oldfiles()<CR>", 'opened files history'},
   },
 })
 
--- nvim/nvim/lua/lsp.lua
+++ nvim/nvim/lua/lsp.lua
@@ -8,11 +8,11 @@ require('go').setup({})
 -- null-ls
 local null_ls = require("null-ls")
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint, -- eslint or eslint_d
-        null_ls.builtins.code_actions.eslint, -- eslint or eslint_d
-        null_ls.builtins.formatting.prettier -- prettier, eslint, eslint_d, or prettierd
-    },
+  sources = {
+    null_ls.builtins.diagnostics.eslint, -- eslint or eslint_d
+    null_ls.builtins.code_actions.eslint, -- eslint or eslint_d
+    null_ls.builtins.formatting.prettier, -- prettier, eslint, eslint_d, or prettierd
+  },
 })
 
 local nvim_lsp = require('lspconfig')
@@ -20,33 +20,30 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
   -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  local wk = require('which-key')
+  wk.register({
+    ['gD'] = {'<cmd>lua vim.lsp.buf.declaration()<CR>', 'Declaration'},
+    ['gd'] = {'<cmd>lua vim.lsp.buf.definition()<CR>', 'Definition'},
+    ['K'] = {'<cmd>lua vim.lsp.buf.hover()<CR>', 'Hover'},
+    ['gi'] = {'<cmd>lua vim.lsp.buf.implementation()<CR>', 'Implementation'},
+    ['<C-k>'] = {'<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature help'},
+    ['<space>wa'] = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', 'Add workspace folder'},
+    ['<space>wr'] = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', 'Remove workspace folder'},
+    ['<space>wl'] = {'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', 'List workspace folders'},
+    ['<space>D'] = {'<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Type definition'},
+    ['<space>rn'] = {'<cmd>lua vim.lsp.buf.rename()<CR>', 'Rename'},
+    ['<space>ca'] = {'<cmd>lua vim.lsp.buf.code_action()<CR>', 'Code Action'},
+    ['gr'] = {'<cmd>lua vim.lsp.buf.references()<CR>', 'References'},
+    ['<space>e'] = {'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', 'Show line diagnostics'},
+    ['[d'] = {'<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', 'Go to diagnostic prev'},
+    [']d'] = {'<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', 'Go to diagnostic next'},
+    ['<space>q'] = {'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', 'Set diagnostic loclist'},
+    ['<space>f'] = {'<cmd>lua vim.lsp.buf.formatting()<CR>', 'Format'},
+  }, { buffer = bufnr })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
