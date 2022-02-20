33d712ea1cdfa2aeb8402bc2cdeb31cfb792ff88
Giovan Isa Musthofa <giovanmail@gmail.com>
2022-02-20 18:29:05 +0700

    nvim: Configure nvim-cmp sources
    Signed-off-by: Giovan Isa Musthofa <giovanmail@gmail.com>

--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -34,7 +34,6 @@ local packer = require('packer').startup(function(use)
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
-      'hrsh7th/nvim-cmp',
       'hrsh7th/cmp-vsnip',
       'hrsh7th/vim-vsnip',
     }
@@ -124,11 +123,9 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
+    { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'cmdline' },
     { name = 'buffer' },
   })
 })
